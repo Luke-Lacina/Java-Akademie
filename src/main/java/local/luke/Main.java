16314ff9737101c50af95e39b4bf9fc9355f5f51
Lukáš Lacina <luke.lacina@gmail.com>
2024-09-30 14:37:32 +0200

    Přidány přístupové metody

--- src/main/java/local/luke/Main.java
+++ src/main/java/local/luke/Main.java
@@ -3,15 +3,15 @@ package local.luke;
 public class Main {
     public static void main(String[] args) {
         ProdejceMrkve prodejce1 = new ProdejceMrkve();
-        prodejce1.jmeno = "Karel Novák";
-        prodejce1.mestoSidlaProdejce = "Znojmo";
+        prodejce1.setJmeno("Karel Novák");
+        prodejce1.setMestoSidlaProdejce("Znojmo");
 
         ProdejceMrkve prodejce2 = new ProdejceMrkve();
-        prodejce2.jmeno = "Jan Dlouhý";
-        prodejce2.mestoSidlaProdejce = "Olomouc";
+        prodejce2.setJmeno("Jan Dlouhý");
+        prodejce2.setMestoSidlaProdejce("Olomouc");
 
         System.out.println("Prodejci Mrkve:");
-        System.out.println(prodejce1.jmeno + ", " + prodejce1.mestoSidlaProdejce);
-        System.out.println(prodejce2.jmeno + ", " + prodejce2.mestoSidlaProdejce);
+        System.out.println(prodejce1.getJmeno() + ", " + prodejce1.getMestoSidlaProdejce());
+        System.out.println(prodejce2.getJmeno() + ", " + prodejce2.getMestoSidlaProdejce());
     }
 }
--- src/main/java/local/luke/ProdejceMrkve.java
+++ src/main/java/local/luke/ProdejceMrkve.java
@@ -3,11 +3,60 @@ package local.luke;
 import java.time.LocalDate;
 
 public class ProdejceMrkve {
-String jmeno;
-    LocalDate datumNarozeniProdejce;
-    int sjednanychSmluv;
-    double prodanychTun;
-    String mestoSidlaProdejce;
-    String registracniZnackaVozidla;
-    double spotrebaVozidla;
+    private String jmeno;
+    private LocalDate datumNarozeniProdejce;
+    private int sjednanychSmluv;
+    private double prodanychTun;
+    private String mestoSidlaProdejce;
+    private String registracniZnackaVozidla;
+    private double spotrebaVozidla;
+
+    public String getJmeno() {
+    return jmeno;
+}
+public void setJmeno(String jmeno) {
+    this.jmeno = jmeno;
+}
+
+    public LocalDate getDatumNarozeniProdejce() {
+        return datumNarozeniProdejce;
+    }
+    public void setDatumNarozeniProdejce(LocalDate datumNarozeniProdejce) {
+        this.datumNarozeniProdejce = datumNarozeniProdejce;
+    }
+
+    public int getSjednanychSmluv() {
+        return sjednanychSmluv;
+    }
+    public void setSjednanychSmluv(int sjednanychSmluv) {
+        this.sjednanychSmluv = sjednanychSmluv;
+    }
+
+    public double getProdanychTun() {
+        return prodanychTun;
+    }
+    public void setProdanychTun(double prodanychTun) {
+        this.prodanychTun = prodanychTun;
+    }
+
+    public String getMestoSidlaProdejce() {
+        return mestoSidlaProdejce;
+    }
+    public void setMestoSidlaProdejce(String mestoSidlaProdejce) {
+        this.mestoSidlaProdejce = mestoSidlaProdejce;
+    }
+
+    public String getRegistracniZnackaVozidla() {
+        return registracniZnackaVozidla;
+    }
+    public void setRegistracniZnackaVozidla(String registracniZnackaVozidla) {
+        this.registracniZnackaVozidla = registracniZnackaVozidla;
+    }
+
+    public double getSpotrebaVozidla() {
+        return spotrebaVozidla;
+    }
+    public void setSpotrebaVozidla(double spotrebaVozidla) {
+        this.spotrebaVozidla = spotrebaVozidla;
+    }
 }
